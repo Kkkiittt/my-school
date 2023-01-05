603c6e2d5ef1fa5167f774021b4a1b018057d311
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2023-01-05 16:32:00 +0500

    created dbcontext

---
+++ src/MySchool.Api/Configurations/DataAccessConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySchool.DataAccess.DbContexts;
+
+namespace My_School.Configurations
+{
+    public static class DataAccessConfiguration
+    {
+        public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+        {
+            string connectionString = builder.Configuration.GetConnectionString("DataBaseConnection");
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        } 
+    }
+}
