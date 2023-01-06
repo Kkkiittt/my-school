9c5e1c212581ac0266163bff59b6fad63b5f98bd
Kkkiittt <khamidov357@gmail.com>
2023-01-06 18:02:33 +0500

    dtos

---
+++ src/MySchool.Services/Dtos/Students/StudentLoginDto.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MySchool.Services.Dtos.Students;
+
+public class StudentLoginDto
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public int Pin { get; set; }
+
+}
