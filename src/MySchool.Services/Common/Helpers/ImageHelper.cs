cc79c4ebdb6568705daed1ae888ddb58cba14c9b
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2023-01-07 15:23:21 +0500

    Created FileService

---
+++ src/MySchool.Services/Common/Helpers/ImageHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace MySchool.Services.Common.Helpers;
+
+public class ImageHelper
+{
+	public static string MakeImageName(string filename)
+	{
+		string extension = Path.GetExtension(filename);
+		string name = "IMG_" + Guid.NewGuid().ToString();
+		return name + extension;
+	}
+}
