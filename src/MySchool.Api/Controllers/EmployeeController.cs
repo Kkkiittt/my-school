a587a3570b162a6a5bb923f2c32c653580e17288
Kkkiittt <khamidov357@gmail.com>
2023-01-07 19:14:44 +0500

    clean code

--- src/MySchool.Api/Controllers/EmployeeController.cs
+++ src/MySchool.Api/Controllers/EmployeeController.cs
@@ -3,42 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 using MySchool.Services.Dtos.Employees;
 using MySchool.Services.Interfaces;
 
-namespace My_School.Controllers
+namespace My_School.Controllers;
+
+[Route("api/employee")]
+[ApiController]
+public class EmployeeController : ControllerBase
 {
-	[Route("api/employee")]
-	[ApiController]
-	public class EmployeeController : ControllerBase
+	private readonly IEmployeeService _employeeService;
+
+	public EmployeeController(IEmployeeService employeeService)
 	{
-		private readonly IEmployeeService _employeeService;
+		_employeeService = employeeService;
+	}
 
-		public EmployeeController(IEmployeeService employeeService)
-		{
-			_employeeService = employeeService;
-		}
+	[HttpPost("EmployeeRegister")]
+	public async Task<IActionResult> RegisterAsync([FromForm] EmployeeRegisterDto dto)
+	{
+		return Ok(await _employeeService.RegisterAsync(dto));
+	}
 
-        [HttpPost("EmployeeRegister")]
-        public async Task<IActionResult> RegisterAsync([FromForm] EmployeeRegisterDto dto)
-        {
-            return Ok(await _employeeService.RegisterAsync(dto));
-        }
+	[HttpPost("EmployeeLogin")]
+	public async Task<IActionResult> LoginAsync([FromForm] EmployeeLoginDto dto)
+	{
+		return Ok(await _employeeService.LoginAsync(dto));
+	}
 
-        [HttpPost("EmployeeLogin")]
-        public async Task<IActionResult> LoginAsync([FromForm] EmployeeLoginDto dto)
-        {
-            return Ok(await _employeeService.LoginAsync(dto));
-        }
-
-        [HttpDelete("DeleteEmployee")]
-        public async Task<IActionResult> DeleteByIdAsync(long id)
-        {
-            return Ok(await _employeeService.DeleteByIdAsync(id));
-        }
+	[HttpDelete("DeleteEmployee")]
+	public async Task<IActionResult> DeleteByIdAsync(long id)
+	{
+		return Ok(await _employeeService.DeleteByIdAsync(id));
+	}
 
 
-        [HttpPut("MakeAuthor")]
-        public async Task<IActionResult> MakeAuthorAsync(long id)
-        {
-            return Ok(await _employeeService.MakeAuthor(id));
-        }
-    }
+	[HttpPut("MakeAuthor")]
+	public async Task<IActionResult> MakeAuthorAsync(long id)
+	{
+		return Ok(await _employeeService.MakeAuthor(id));
+	}
 }
