d5a54c6b3c91dce3fe7c2ae9075fb62a0ac716b2
shohruxbek zokirjonov <davok1n@outlook.com>
2023-01-07 16:54:37 +0500

    Create Employee & student Controllers

---
+++ src/MySchool.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySchool.Services.Dtos.Employees;
+using MySchool.Services.Interfaces;
+
+namespace My_School.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost(" Employee Register")]
+
+        public async Task<IActionResult> RegisterAsync([FromForm] EmployeeRegisterDto dto)
+        {
+            return Ok(await _employeeService.RegisterAsync(dto));
+        }
+    }
+}
