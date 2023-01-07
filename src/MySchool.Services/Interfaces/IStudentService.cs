d14874ef99b9e33bf1cb7ebf4ea1d0a8475f2eb6
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2023-01-07 16:48:44 +0500

    Added Interface

--- src/MySchool.Services/Interfaces/IStudentService.cs
+++ src/MySchool.Services/Interfaces/IStudentService.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MySchool.Services.Dtos.Students;
+using MySchool.Services.ViewModels.Students;
 
 namespace MySchool.Services.Interfaces;
 
 public interface IStudentService
 {
-	public Task<bool> RegisterAsync(StudentRegisterDto dto);
+	public Task<StudentRegisterViewModel> RegisterAsync(StudentRegisterDto dto);
+
 	public Task<string> LoginAsync(StudentLoginDto dto);
+
 	public Task<bool> DeleteByIdAsync(long id);
 }
