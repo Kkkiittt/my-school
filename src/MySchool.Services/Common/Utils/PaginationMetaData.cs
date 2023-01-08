1eef33c9eec256e9affeb9e052ca41e7ce9a3189
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2023-01-08 15:26:41 +0500

    CreatedPagination

---
+++ src/MySchool.Services/Common/Utils/PaginationMetaData.cs
@@ -0,0 +1,17 @@
+﻿namespace MySchool.Services.Common.Utils;
+
+public class PaginationMetaData
+{
+	public int CurrentPage { get; set; }
+
+	public int TotalPages { get; set; }
+
+	public bool HasNext { get; set; }
+
+	public bool HasPrevious { get; set; }
+
+	public int PageSize { get; set; }
+
+	public int TotalItems { get; set; }
+
+}
---
+++ src/MySchool.Services/Service/PaginatorService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using MySchool.Services.Common.Utils;
+using MySchool.Services.Interfaces;
+using Newtonsoft.Json;
+
+namespace MySchool.Services.Service;
+
+public class PaginatorService : IPaginatorService
+{
+	private readonly IHttpContextAccessor _accessor;
+
+	public PaginatorService(IHttpContextAccessor httpContextAccessor)
+	{
+		this._accessor = httpContextAccessor;
+	}
+	public async Task<IList<T>> ToPagedAsync<T>(IQueryable<T> items, int pageNumber, int pageSize)
+	{
+		int totalitems = await items.CountAsync();
+
+
+		PaginationMetaData paginationMetaData = new PaginationMetaData()
+		{
+			CurrentPage = pageNumber,
+			PageSize = pageSize,
+			TotalItems = totalitems,
+			TotalPages = (int)Math.Ceiling((double)totalitems / (double)pageSize),
+			HasPrevious = pageNumber > 0,
+		};
+		paginationMetaData.HasNext = paginationMetaData.CurrentPage < paginationMetaData.TotalPages;
+
+
+
+		string json = JsonConvert.SerializeObject(paginationMetaData);
+
+		_accessor.HttpContext.Response.Headers.Add("X-Pagination", json);
+
+		return await items.Skip(pageNumber * pageSize - pageSize)
+						  .Take(pageSize)
+						  .ToListAsync();
+
+	}
+}
