86f68366d8219b4a373deb87ef2d4c4afc341c60
Kkkiittt <khamidov357@gmail.com>
2023-01-08 21:22:39 +0500

    phone service

---
+++ src/MySchool.Services/Interfaces/Services/IPhoneService.cs
@@ -0,0 +1,10 @@
+﻿using MySchool.Services.Dtos.Common;
+
+namespace MySchool.Services.Interfaces.Common;
+
+public interface IPhoneService
+{
+	public Task<bool> SendCode(long authorId);
+
+	public Task<bool> ConfirmCode(CodeConfirmDto dto);
+}
