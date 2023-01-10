d9cbaf9d3615f36391ef37126cdee2b42a3a8adf
Kkkiittt <khamidov357@gmail.com>
2023-01-10 12:27:34 +0500

    getAll added to employeeService

--- src/MySchool.Services/Interfaces/Services/IEmployeeService.cs
+++ src/MySchool.Services/Interfaces/Services/IEmployeeService.cs
@@ -1,9 +1,12 @@
 ﻿using MySchool.Services.Dtos.Employees;
+using MySchool.Services.ViewModels.Employees;
 
 namespace MySchool.Services.Interfaces;
 
 public interface IEmployeeService
 {
+	public Task<IEnumerable<EmployeeShortViewModel>> GetAll();
+
 	public Task<bool> RegisterAsync(EmployeeRegisterDto dto);
 
 	public Task<string> LoginAsync(EmployeeLoginDto dto);
